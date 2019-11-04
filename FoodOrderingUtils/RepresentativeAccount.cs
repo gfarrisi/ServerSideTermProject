37bb6ab01af18af016ba94f03cd865ae0681cb7c
Hazel Boston <paulboston42@gmail.com>
2019-11-04 09:42:51 -0500

    Library for account and restaurant objects

---
+++ FoodOrderingUtils/RepresentativeAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodOrderingUtils
+{
+    public class RepresentativeAccount
+    {
+        int RestaurantID { get; set; }
+        string Email { get; set; }
+        string Password { get; set; }
+        string RepresentativeName { get; set; }
+        string Address1 { get; set; }
+        string Address2 { get; set; }
+        string City { get; set; }
+        string State { get; set; }
+        string Zip { get; set; }
+    }
+}
