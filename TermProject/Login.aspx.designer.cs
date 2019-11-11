5040353d08cb0e02fe86a4cff5fc7a0724382de3
gfarrisi <gabriellafarrisi@gmail.com>
2019-11-11 09:43:09 -0500

    created loging and customer registration pages

--- TermProject/Login.aspx.designer.cs
+++ TermProject/Login.aspx.designer.cs
@@ -13,6 +13,33 @@ namespace TermProject {
     public partial class Default {
         
         /// <summary>
+        /// txtEmail control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+        
+        /// <summary>
+        /// txtPassword control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPassword;
+        
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
         /// lbForgotEmail control.
         /// </summary>
         /// <remarks>
