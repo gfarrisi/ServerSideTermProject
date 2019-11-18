b153d330871ba2ecd1022fa051f8d2a1ec87ba34
Hazel Boston <paulboston42@gmail.com>
2019-11-18 11:53:02 -0500

    Restaurant Control

---
+++ TermProject/LandingPage.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/FoodOrder.Master" AutoEventWireup="true" CodeBehind="LandingPage.aspx.cs" Inherits="TermProject.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+     <form id="form1" class="contact-form" runat="server">
+         <h2 style="text-align:center;">Restaurants List</h2>
+         <br />
+         <div class="container">
+             <div class="col-md-8" id="divRestaurantColumn" runat="server">
+
+
+
+
+             </div>
+             <div class="col-md-6">
+
+             </div>
+
+         </div>
+         </form>
+</asp:Content>
---
+++ TermProject/LandingPage.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TermProject
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                RestaurantControl ctrl = (RestaurantControl)LoadControl("RestaurantControl.ascx");
+                ctrl.ResTitle = "Restauraunt " + i;
+                ctrl.ResDescription = "Description";
+                ctrl.ResAddress = "1252 Main Road";
+                ctrl.ResImage = "img/trending/trending-" + i + ".jpg";
+                //ctrl.DataBind();
+                divRestaurantColumn.Controls.Add(ctrl);
+            }
+        }
+    }
+}
