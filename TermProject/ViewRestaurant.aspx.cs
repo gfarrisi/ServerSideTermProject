17b4ef3844b04b5eae799cf5736638fa0d6af270
Hazel Boston <hazelpboston@gmail.com>
2019-11-24 14:46:22 -0500

    Restaurant individual page and menu item controls in progress.

---
+++ TermProject/ViewRestaurant.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Utilities;
+
+namespace TermProject
+{
+    public partial class ViewRestaurant : System.Web.UI.Page
+    {
+        DBConnect objDB = new DBConnect();
+        DataSet ds;
+        int RestaurantID = 400;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                SqlCommand sqlGetMenu = new SqlCommand();
+                sqlGetMenu.CommandType = CommandType.StoredProcedure;
+                sqlGetMenu.CommandText = "TP_GetMenuItems";
+                sqlGetMenu.Parameters.AddWithValue("@RestaurantID", RestaurantID);
+                ds = objDB.GetDataSetUsingCmdObj(sqlGetMenu);
+            }
+        }
+    }
+}
